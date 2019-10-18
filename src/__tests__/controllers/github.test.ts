bdb308bfba5214734d65fdeef0a078caf102d7d3
Luiz Kota <luiz.kota@catho.com>
2019-10-18 11:24:39 -0300

    test(unit-test): add unit test to Github Controller

---
+++ src/__tests__/controllers/github.test.ts
@@ -0,0 +1,34 @@
+import { Response, Request } from 'express';
+import Github from '../../controllers/github';
+
+const mockResponse = {
+  redirect: jest.fn(),
+};
+
+const mockRequest = {
+  query: {
+    code: '123',
+  },
+};
+
+describe(
+  'Test the Github Controller',
+  () => {
+    it(
+      'Testing the mainAction',
+      () => {
+        // @ts-ignore
+        const responseMock: Response = { ...mockResponse };
+        // @ts-ignore
+        const requestMock: Request = { ...mockRequest };
+
+        Github.mainAction(requestMock, responseMock);
+
+        expect(responseMock.redirect)
+          .toBeCalledTimes(1);
+        expect(responseMock.redirect)
+          .toHaveBeenCalledWith('/user/123/');
+      },
+    );
+  },
+);
